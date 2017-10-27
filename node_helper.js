b937145e767fe226e73a71fab8ded69b4909102b
Emil Breding <emil.breding@gmail.com>
2017-10-27 19:11:13 +0200

    Added cached responses and filtering out already passed departures

--- node_helper.js
+++ node_helper.js
@@ -5,7 +5,9 @@ var moment = require('moment');
 
 module.exports = NodeHelper.create({
 
-    reloadInterval: 5 * 60 * 1000,
+    reloadInterval: 10 * 60 * 1000,
+    refreshInterval: 60 * 1000,
+    lastFetchedDepartures: [],
 
     // Subclass start method.
     start: function () {
@@ -17,19 +19,44 @@ module.exports = NodeHelper.create({
         if (notification === 'SL_INIT') {
             this.config = payload;
             this.reloadTimer = null;
+            this.updateTimer = null;
             this.fetchDepartures();
         }
+    },
+
+    updateView: function() {
+        this.scheduleUpdateTimer();
+
+        console.log('Updating buses view!');
+        var departures = [];
+        // Filter already departured buses
+        var now = moment.now();
+        var buses = this.lastFetchedDepartures.filter(bus => {
+            return moment(bus.ExpectedDateTime).isAfter(now);
+        });
+
+        buses = buses.slice(0, 7);
+        buses.forEach(bus => {
+            departures.push({
+                line_number: bus.LineNumber,
+                display_time: bus.DisplayTime,
+                expected_time: moment(bus.ExpectedDateTime).format('HH:mm'),
+                destination: bus.Destination
+            });
+        });
+
+        this.sendSocketNotification("SL_DEPARTURES", departures);
     },
 
     fetchDepartures: function () {
 
         const self = this;
-        departures = [];
+        
         var url = this.config.api_url.replace('API_KEY', this.config.api_key).replace('TIME_WINDOW', this.config.time_window).replace('STATION', this.config.station);
 
         nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
         headers = { "User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version + " (https://github.com/MichMich/MagicMirror/)" }
-
+        console.log(url);
         request({ uri: url, encoding: null, headers: headers }, function (error, response, body) {
 
             if (error || response.statusCode != 200) {
@@ -47,17 +74,9 @@ module.exports = NodeHelper.create({
                         bus.Destination !== 'Skärgårdsstad'
                 });
 
-                suitableBuses = suitableBuses.slice(0, 7);
-                suitableBuses.forEach(bus => {
-                    departures.push({
-                        line_number: bus.LineNumber,
-                        display_time: bus.DisplayTime,
-                        expected_time: moment(bus.ExpectedDateTime).format('HH:mm'),
-                        destination: bus.Destination
-                    });
-                });
-
-                self.sendSocketNotification("SL_DEPARTURES", departures);
+                self.lastFetchedDepartures = suitableBuses;
+                self.updateView();
+                
             } catch (error) {
                 console.log('Something went wrong when parsing response from SL: ' + error);
             }
@@ -71,5 +90,13 @@ module.exports = NodeHelper.create({
         this.reloadTimer = setTimeout(function () {
             self.fetchDepartures();
         }, this.reloadInterval);
-    }
+    },
+
+    scheduleUpdateTimer: function () {
+        var self = this;
+        clearTimeout(this.updateTimer);
+        this.updateTimer = setTimeout(function () {
+            self.updateView();
+        }, this.refreshInterval);
+    },
 });
